454720b43386b5a13b8a754fdf46ab22b7b582d3
mirangee <pmrang@live.co.kr>
2024-02-19 00:04:13 +0900

    구현 완 (단, hover 기능 이상 해결 못함)

--- assets/scripts/startGame.js
+++ assets/scripts/startGame.js
@@ -1,25 +1,99 @@
 export default function startGame() {
 
     //1. 숫자 볼 화면에 띄우기
+    
     makeIconList();
-    //2. 랜덤숫자 하나 생성
 
-    //3. 숫자 볼에 click 이벤트 발생하면 랜덤숫자와 클릭된 숫자 비교(클릭된 횟수 저장하기)
+    //2. 랜덤숫자 하나 생성
+    const range = 100 // 1~range 숫자로 범위 설정
+    const randomNumber = Math.round(Math.random() * (range - 1)) + 1;
+    console.log(randomNumber);
+
+    //3. 숫자 볼에 click 이벤트 리스너 생성 및 클릭된 숫자 저장(클릭된 횟수 저장하기)
+    let clickCount = 0;
+
+    const clickEventHandler = (e) => {
+        if (!e.target.matches('#numbers .icon')) {
+            return;
+        }
+        clickCount++;
+        let iconNumber = +e.target.dataset.value;
+        compareNumbers(iconNumber);
+    }
+
+    $numbers.addEventListener('click', clickEventHandler);
+
+    
 
     //4. 숫자 비교하여 결과값 도출(UP, DOWN, WIN)
-    // UP 이면 UP 아이콘 움직이기, h2의 begin 숫자 수정하고, 해당 숫자 이하 숫자 볼 삭제
-    // DOWN 이면 DOWN 아이콘 움직이기, h2의 end 숫자 수정하고, 해당 숫자 이상 숫자 볼 삭제
-    // WIN 이면 win 클래스의 성공 문구 보여주기
-    
-     
+    function compareNumbers(pickedNumber) {
+
+        if (randomNumber === pickedNumber) {
+            // 1. .win 보이면서 몇 번만에 맞혔는지 보여주기
+            $win.style.opacity = 1;
+            $chance.textContent = clickCount;
+
+            // 2.  up, down 아이콘 움직임 멈추기
+            $up.classList.remove('selected');
+            $down.classList.remove('selected');
+
+            // 3. 클릭해도 동작 안 하도록 이벤트 리스너 멈추기
+            $numbers.removeEventListener('click', clickEventHandler);
+            
+        } else if (randomNumber > pickedNumber) {
+            console.log('UP!!');
+            // 1. #begin 숫자를 pickedNumber+1 로 수정
+            $begin.textContent = pickedNumber + 1;
+
+            // 2. pickedNumber 이하 숫자볼 삭제
+            let numToRemove = pickedNumber; // 삭제해야할 볼 개수 저장
+            const $icons = document.querySelectorAll('.icon');
+            for(let $i of $icons) {
+                if (numToRemove === 0) {
+                    break;
+                } else if ($i.dataset.value <= pickedNumber) {
+                    $numbers.removeChild($i);
+                    numToRemove--; // 삭제해야할 볼 개수 차감
+                }
+            }
+
+            // 3. UP 아이콘 움직이기
+            $up.classList.add('selected');
+            $down.classList.remove('selected');
+
+        } else {
+            console.log('Down!');
+            // 1. #end 숫자를 pickedNumber-1 로 수정
+            $end.textContent = pickedNumber - 1;
+
+            // 2. pickedNumber 이상 숫자볼 삭제
+            let numToRemove = range - pickedNumber + 1; // 전체범위에서 자신 포함(+1하는 이유) 이상의 개수를 삭제해야 함
+            const $icons = document.querySelectorAll('.icon');
+            for(let $i of $icons) {
+                if (numToRemove === 0) {
+                    break;
+                } else if ($i.dataset.value >= pickedNumber) {
+                    $numbers.removeChild($i);
+                    numToRemove--;
+                }
+            }
+            
+            // 3. UP 아이콘 움직이기
+            $down.classList.add('selected');
+            $up.classList.remove('selected');
+        }
+    }
 }
+
+
 
 function makeIconList() {
     const $frag = document.createDocumentFragment();
-    for(let i = 1; i <= 100; i++) {
+    for (let i = 1; i <= 100; i++) {
         const $numberBall = document.createElement('div');
         $numberBall.classList.add('icon');
         $numberBall.textContent = `${i}`;
+        $numberBall.dataset.value = i;
         $frag.appendChild($numberBall);
     }
     $numbers.appendChild($frag);
--- assets/scripts/vendor.js
+++ assets/scripts/vendor.js
@@ -3,3 +3,12 @@
 // 필요한 요소 취득
 
 const $numbers = document.getElementById('numbers');
+
+const $begin = document.getElementById('begin');
+const $end = document.getElementById('end');
+
+const $up = document.querySelector('.up');
+const $down = document.querySelector('.down');
+
+const $win = document.querySelector('.win');
+const $chance = document.getElementById('chance');
